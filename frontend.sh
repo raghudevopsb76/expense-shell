4611cbad31e5e409a4ef7bc36ce0aad2ecb13c70
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-02 06:48:14 +0530

    Adding Shell Topics

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,25 @@
-echo -e "\e[36m Installing Nginx \e[0m"
-dnf install nginx -y &>>/tmp/expense.log
+log_file=/tmp/expense.log
+color="\e[36m"
 
-echo -e "\e[36m Copy Expense Config file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+echo -e "${color} Installing Nginx \e[0m"
+dnf install nginx -y &>>$log_file
 
-echo -e "\e[36m Clean Old Nginx Content \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+echo -e "${color} Copy Expense Config file \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
-echo -e "\e[36m Download Frontend Application Code \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+echo -e "${color} Clean Old Nginx Content \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
-echo -e "\e[36m Extract Downloaded Application Content \e[0m"
-cd /usr/share/nginx/html &>>/tmp/expense.log
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+echo -e "${color} Download Frontend Application Code \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 
-echo -e "\e[36m Starting Nginx Service \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
-systemctl restart nginx &>>/tmp/expense.log
+echo -e "${color} Extract Downloaded Application Content \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+
+echo -e "${color} Starting Nginx Service \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 
 
 
