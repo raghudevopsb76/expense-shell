c9f11cf373a0105efb6bb98649d964c264ad054b
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-02 07:23:47 +0530

    Adding Shell Topics

--- backend.sh
+++ backend.sh
@@ -25,6 +25,10 @@ echo -e "${color} Create Application Directory \e[0m"
 mkdir /app &>>$log_file
 echo $?
 
+echo -e "${color} Delete old Application Content \e[0m"
+rm -rf /app/* &>>$log_file
+echo $?
+
 echo -e "${color} Download Application Content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 echo $?
