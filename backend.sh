0677425871886ae7656d429a9d682c336609b60f
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-03 06:31:42 +0530

    Adding Shell Topics

--- backend.sh
+++ backend.sh
@@ -3,7 +3,11 @@ color="\e[33m"
 
 echo -e "${color} Disable NodeJS default Version \e[0m"
 dnf module disable nodejs -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} Enable NodeJS 18 Version \e[0m"
 dnf module enable nodejs:18 -y &>>$log_file
