3d34f6e1dcbb390db9b1e0feefec1259221a3252
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-03 06:45:24 +0530

    Adding Shell Topics

--- backend.sh
+++ backend.sh
@@ -33,13 +33,17 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "${color} Add Application User \e[0m"
-useradd expense &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
+id expense &>>$log_file
+if [ $? -ne 0 ]; then
+  echo -e "${color} Add Application User \e[0m"
+  useradd expense &>>$log_file
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
 fi
+
 
 echo -e "${color} Create Application Directory \e[0m"
 mkdir /app &>>$log_file
