4437a428bff0bf5cd972637b9f9d3f46c8054710
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-03 06:48:54 +0530

    Adding Shell Topics

--- backend.sh
+++ backend.sh
@@ -44,13 +44,14 @@ if [ $? -ne 0 ]; then
   fi
 fi
 
-
-echo -e "${color} Create Application Directory \e[0m"
-mkdir /app &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
+if [ -d /app ]; then
+  echo -e "${color} Create Application Directory \e[0m"
+  mkdir /app &>>$log_file
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
 fi
 
 echo -e "${color} Delete old Application Content \e[0m"
